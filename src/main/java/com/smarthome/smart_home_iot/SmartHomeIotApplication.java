e3de13da605cb3cfb1a82eee10e569cef4c23b78
sangyong <bagsangyong34@gmail.com>
2025-11-21 16:40:35 +0900

    Initial Commit

---
+++ src/main/java/com/smarthome/smart_home_iot/SmartHomeIotApplication.java
@@ -0,0 +1,13 @@
+package com.smarthome.smart_home_iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeIotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeIotApplication.class, args);
+	}
+
+}
