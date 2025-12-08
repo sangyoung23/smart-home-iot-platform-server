c920903d6d1321b8083da96af3ef5c2af64dd1ea
sangyong <bagsangyong34@gmail.com>
2025-12-08 21:16:36 +0900

    feature: HumidityKafkaMessageDto Kafka 메시지 DTO 추가

---
+++ src/main/java/com/smarthome/smart_home_iot/dto/kafka/HumidityKafkaMessageDto.java
@@ -0,0 +1,17 @@
+package com.smarthome.smart_home_iot.dto.kafka;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+public class HumidityKafkaMessageDto {
+    // 습도 DTO
+
+    private Long id; // 고유 id
+    private String deviceId; // 디바이스 ID
+    private Double humidity; // 습도
+    private LocalDateTime timestamp; // 데이터가 측정된 시간
+
+}
