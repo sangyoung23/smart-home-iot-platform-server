52c4e30f04b0154f27eef864d669f960c4533131
sangyong <bagsangyong34@gmail.com>
2025-12-08 21:18:38 +0900

    feature: KafkaRepository JPA Repository 제거

--- src/main/java/com/smarthome/smart_home_iot/repository/KafkaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.smarthome.smart_home_iot.repository;
-
-import com.smarthome.smart_home_iot.domain.Sensor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KafkaRepository extends JpaRepository<Sensor, Long> {
-
-}
