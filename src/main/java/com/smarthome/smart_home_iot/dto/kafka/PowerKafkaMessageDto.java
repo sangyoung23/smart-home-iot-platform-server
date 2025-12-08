ae94b0543868158b8d0a9ea6d6db573c12bcda1e
sangyong <bagsangyong34@gmail.com>
2025-12-08 21:16:48 +0900

    feature: PowerKafkaMessageDto Kafka 메시지 DTO 추가

---
+++ src/main/java/com/smarthome/smart_home_iot/dto/kafka/PowerKafkaMessageDto.java
@@ -0,0 +1,19 @@
+package com.smarthome.smart_home_iot.dto.kafka;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+public class PowerKafkaMessageDto {
+    // 전력 DTO
+
+    private Long id; // 고유 id
+    private String deviceId; // 디바이스 ID
+    private Double powerUsage; // 전력 사용량(W)
+    private Double voltage; // 전압(V)
+    private Double current; // 전류(A)
+    private Double energyTotal; // 누적 전력(kWh)
+    private LocalDateTime timestamp; // 데이터가 측정된 시간
+}
