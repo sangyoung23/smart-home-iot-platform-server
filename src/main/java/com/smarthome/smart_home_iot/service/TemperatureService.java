0e293f9cdd755cbd8719b68300630c2e459c1e80
sangyong <bagsangyong34@gmail.com>
2025-12-09 19:05:42 +0900

    feature: 센서 service Mongo, Redis 저장 로직 추가

--- src/main/java/com/smarthome/smart_home_iot/service/TemperatureService.java
+++ src/main/java/com/smarthome/smart_home_iot/service/TemperatureService.java
@@ -2,10 +2,11 @@ package com.smarthome.smart_home_iot.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.smarthome.smart_home_iot.domain.TemperatureSensor;
+import com.smarthome.smart_home_iot.document.TemperatureDocument;
 import com.smarthome.smart_home_iot.dto.kafka.TemperatureKafkaMessageDto;
-import com.smarthome.smart_home_iot.repository.TemperatureSensorRepository;
+import com.smarthome.smart_home_iot.repository.mongo.TemperatureDocumentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -14,21 +15,31 @@ import org.springframework.stereotype.Service;
 public class TemperatureService {
 
     private final ObjectMapper objectMapper;
-    private final TemperatureSensorRepository temperatureSensorRepository;
+    private final TemperatureDocumentRepository temperatureDocumentRepository;
+    private final StringRedisTemplate stringRedisTemplate;
 
     @KafkaListener(topics = "temperature-topic", groupId = "sensor-group")
     public void temperatureConsume(String message) {
         try {
+
+            // 1. Kafka 메세지 DTO 직렬화
             TemperatureKafkaMessageDto tempDto = objectMapper.readValue(message, TemperatureKafkaMessageDto.class);
 
-            TemperatureSensor tempEntity = new TemperatureSensor();
-            tempEntity.setDeviceId(tempDto.getDeviceId());
-            tempEntity.setTemperature(tempDto.getTemperature());
-            tempEntity.setTimestamp(tempDto.getTimestamp());
+            // 2. MongoDB Document Raw 데이터 저장
+            TemperatureDocument tempDoc = new TemperatureDocument();
+            tempDoc.setDeviceId(tempDto.getDeviceId());
+            tempDoc.setTemperature(tempDto.getTemperature());
+            tempDoc.setTimestamp(tempDto.getTimestamp());
 
-            temperatureSensorRepository.save(tempEntity);
+            temperatureDocumentRepository.save(tempDoc);
+
+            // 3. Redis 최신 상태 저장
+            String key = "sensor:temperature:" + tempDto.getDeviceId();
+            String value = objectMapper.writeValueAsString(tempDto);
+            stringRedisTemplate.opsForValue().set(key, value);
+
         } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Temperature JSON Parsing Error", e);
         }
     }
 }
