e2719dc436da9cfe81c4780f7b10ebdbfa8fc53b
sangyong <bagsangyong34@gmail.com>
2025-12-09 19:03:50 +0900

    feature: 센서 데이터 Mongo Repo 생성

---
+++ src/main/java/com/smarthome/smart_home_iot/repository/mongo/BatteryDocumentRepository.java
@@ -0,0 +1,7 @@
+package com.smarthome.smart_home_iot.repository.mongo;
+
+import com.smarthome.smart_home_iot.document.BatteryDocument;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface BatteryDocumentRepository extends MongoRepository<BatteryDocument, String> {
+}
---
+++ src/main/java/com/smarthome/smart_home_iot/repository/mongo/PowerDocumentRepository.java
@@ -0,0 +1,7 @@
+package com.smarthome.smart_home_iot.repository.mongo;
+
+import com.smarthome.smart_home_iot.document.PowerDocument;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PowerDocumentRepository extends MongoRepository<PowerDocument, String> {
+}
