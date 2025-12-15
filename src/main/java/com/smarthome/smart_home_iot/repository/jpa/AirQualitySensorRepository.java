e970d387dc282588ce2a11da1a714ba4e144fe59
sangyong <bagsangyong34@gmail.com>
2025-12-15 18:44:39 +0900

    fix: 기존 센서 엔티티 제거

--- src/main/java/com/smarthome/smart_home_iot/repository/jpa/AirQualitySensorRepository.java
+++ src/main/java/com/smarthome/smart_home_iot/repository/jpa/AirQualitySensorRepository.java
@@ -1,6 +1,5 @@
 package com.smarthome.smart_home_iot.repository.jpa;
 
-import com.smarthome.smart_home_iot.domain.AirQualitySensor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AirQualitySensorRepository extends JpaRepository<AirQualitySensor, Long> {
--- src/main/java/com/smarthome/smart_home_iot/repository/jpa/TemperatureSensorRepository.java
+++ src/main/java/com/smarthome/smart_home_iot/repository/jpa/TemperatureSensorRepository.java
@@ -1,6 +1,5 @@
 package com.smarthome.smart_home_iot.repository.jpa;
 
-import com.smarthome.smart_home_iot.domain.TemperatureSensor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TemperatureSensorRepository extends JpaRepository<TemperatureSensor, Long> {
