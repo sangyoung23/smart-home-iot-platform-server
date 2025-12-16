d7ee25da32ed4572a7308cffd6715f8399ee1c71
sangyong <bagsangyong34@gmail.com>
2025-12-16 18:49:11 +0900

    feature: step config TemperatureStepConfig

---
+++ src/main/java/com/smarthome/smart_home_iot/batch/step/TemperatureStepConfig.java
@@ -0,0 +1,35 @@
+package com.smarthome.smart_home_iot.batch.step;
+
+import com.smarthome.smart_home_iot.batch.step.processor.TemperatureStatisticsProcessor;
+import com.smarthome.smart_home_iot.batch.step.reader.TemperatureAggReader;
+import com.smarthome.smart_home_iot.batch.step.writer.TemperatureStatisticsWriter;
+import com.smarthome.smart_home_iot.domain.TemperatureStatistics;
+import com.smarthome.smart_home_iot.dto.batch.TemperatureAggResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.Step;
+import org.springframework.batch.core.step.builder.ChunkOrientedStepBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class TemperatureStepConfig {
+
+    private final TemperatureAggReader temperatureReader;
+    private final TemperatureStatisticsProcessor temperatureProcessor;
+    private final TemperatureStatisticsWriter temperatureWriter;
+
+    @Bean
+    public Step temperatureStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        int chunkSize = 100;
+
+        return new ChunkOrientedStepBuilder<TemperatureAggResult, TemperatureStatistics>("temperatureStep", jobRepository, chunkSize)
+                .transactionManager(transactionManager)
+                .reader(temperatureReader)
+                .processor(temperatureProcessor)
+                .writer(temperatureWriter)
+                .build();
+    }
+}
