399915b62b898fdea575ffae286a1078fd82d286
sangyong <bagsangyong34@gmail.com>
2025-12-16 18:50:22 +0900

    feature: 몽고디비 집계용 HumidityAggResult DTO 생성

---
+++ src/main/java/com/smarthome/smart_home_iot/dto/batch/HumidityAggResult.java
@@ -0,0 +1,23 @@
+package com.smarthome.smart_home_iot.dto.batch;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter @Setter
+public class HumidityAggResult {
+    private String deviceId;
+
+    private LocalDate statDate;
+
+    private int statHour;
+
+    private double avgHumidity;
+
+    private double minHumidity;
+
+    private double maxHumidity;
+
+    private int sampleCount;
+}
