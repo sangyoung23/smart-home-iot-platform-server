18ca30a179ff1c5ccb9adf13d9e30baf3712ec92
sangyong <bagsangyong34@gmail.com>
2025-12-17 15:30:36 +0900

    feature: 센서 데이터 Batch Processor 생성

---
+++ src/main/java/com/smarthome/smart_home_iot/batch/step/processor/AirQualityStatisticsProcessor.java
@@ -0,0 +1,37 @@
+package com.smarthome.smart_home_iot.batch.step.processor;
+
+import com.smarthome.smart_home_iot.domain.AirQualityStatistics;
+import com.smarthome.smart_home_iot.dto.batch.AirQualityAggResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.infrastructure.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class AirQualityStatisticsProcessor
+        implements ItemProcessor<AirQualityAggResult, AirQualityStatistics> {
+
+    @Override
+    public AirQualityStatistics process(AirQualityAggResult item) {
+
+        return AirQualityStatistics.builder()
+                .deviceId(item.getDeviceId())
+                .statDate(item.getStatDate())
+                .statHour(item.getStatHour())
+
+                .avgPm10(item.getAvgPm10())
+                .avgPm25(item.getAvgPm25())
+                .avgCo2(item.getAvgCo2())
+                .avgVoc(item.getAvgVoc())
+                .avgLight(item.getAvgLight())
+
+                .gasLeakCount(item.getGasLeakCount())
+                .avgSmokeLevel(item.getAvgSmokeLevel())
+                .sampleCount(item.getSampleCount())
+
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+}
