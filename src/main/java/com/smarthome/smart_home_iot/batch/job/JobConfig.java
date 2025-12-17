cc498bde2c064afa08b922abc2239cb1a65ea2c4
sangyong <bagsangyong34@gmail.com>
2025-12-17 15:30:05 +0900

    feature: Batch JobConfig 개별 Step 직접 주입

--- src/main/java/com/smarthome/smart_home_iot/batch/job/JobConfig.java
+++ src/main/java/com/smarthome/smart_home_iot/batch/job/JobConfig.java
@@ -1,32 +1,32 @@
 package com.smarthome.smart_home_iot.batch.job;
 
-import com.smarthome.smart_home_iot.batch.step.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.job.Job;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.Step;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.PlatformTransactionManager;
 
-@Configuration
 @RequiredArgsConstructor
+@Configuration
 public class JobConfig {
 
-    private final TemperatureStepConfig temperatureStepConfig;
-//    private final HumidityStepConfig humidityStepConfig;
-//    private final PowerStepConfig powerStepConfig;
-//    private final BatteryStepConfig batteryStepConfig;
-//    private final AirQualityStepConfig airQualityStepConfig;
+    private final Step temperatureStep;
+    private final Step humidityStep;
+    private final Step powerStep;
+    private final Step batteryStep;
+    private final Step airQualityStep;
 
     @Bean
-    public Job sensorJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+    public Job sensorJob(JobRepository jobRepository) {
         return new JobBuilder("sensorJob", jobRepository)
-                .start(temperatureStepConfig.temperatureStep(jobRepository, transactionManager))
-//                .next(humidityStepConfig.humidityStep(jobRepository, transactionManager))
-//                .next(powerStepConfig.powerStep(jobRepository, transactionManager))
-//                .next(batteryStepConfig.batteryStep(jobRepository, transactionManager))
-//                .next(airQualityStepConfig.airQualityStep(jobRepository, transactionManager))
+                .start(temperatureStep)
+                .next(humidityStep)
+                .next(powerStep)
+                .next(batteryStep)
+                .next(airQualityStep)
                 .build();
     }
 }
+
