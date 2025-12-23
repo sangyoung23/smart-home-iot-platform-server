86cc94e6826654c9d140f20b090e5e51d4e693a7
sangyong <bagsangyong34@gmail.com>
2025-12-23 22:46:39 +0900

    feature: Spring 스케줄링 활성화를 위한 설정 클래스 추가

---
+++ src/main/java/com/smarthome/smart_home_iot/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package com.smarthome.smart_home_iot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
