827becb75a9f0aeb69586ba0df568ffcc9421f98
sangyong <bagsangyong34@gmail.com>
2025-12-30 17:59:18 +0900

    feature: 원본 데이터 일괄 처리 완료 마킹 서비스 추가

---
+++ src/main/java/com/smarthome/smart_home_iot/service/batch/TemperatureBulkUpdateService.java
@@ -0,0 +1,29 @@
+package com.smarthome.smart_home_iot.service.batch;
+
+import com.mongodb.client.result.UpdateResult;
+import com.smarthome.smart_home_iot.document.TemperatureDocument;
+import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TemperatureBulkUpdateService {
+
+    private final MongoTemplate mongoTemplate;
+
+    public long markProcessedByIds(List<ObjectId> ids) {
+        Query query = new Query(Criteria.where("_id").in(ids));
+        Update update = new Update().set("isProcessed", true);
+
+        UpdateResult result = mongoTemplate.updateMulti(query, update, TemperatureDocument.class);
+
+        return result.getModifiedCount();
+    }
+}
