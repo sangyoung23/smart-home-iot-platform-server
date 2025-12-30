01c3552957bf6ec687f78e3668f6221335a91657
sangyong <bagsangyong34@gmail.com>
2025-12-30 17:58:31 +0900

    feature:UPSERT를 위한 복합 조회 메서드 추가

--- src/main/java/com/smarthome/smart_home_iot/repository/jpa/AirQualityStatisticsRepository.java
+++ src/main/java/com/smarthome/smart_home_iot/repository/jpa/AirQualityStatisticsRepository.java
@@ -3,5 +3,13 @@ package com.smarthome.smart_home_iot.repository.jpa;
 import com.smarthome.smart_home_iot.domain.AirQualityStatistics;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface AirQualityStatisticsRepository extends JpaRepository<AirQualityStatistics, Long> {
+    Optional<AirQualityStatistics> findByDeviceIdAndStatDateAndStatHour(
+            String deviceId,
+            LocalDate statDate,
+            int statHour
+    );
 }
--- src/main/java/com/smarthome/smart_home_iot/repository/jpa/HumidityStatisticsRepository.java
+++ src/main/java/com/smarthome/smart_home_iot/repository/jpa/HumidityStatisticsRepository.java
@@ -3,5 +3,13 @@ package com.smarthome.smart_home_iot.repository.jpa;
 import com.smarthome.smart_home_iot.domain.HumidityStatistics;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface HumidityStatisticsRepository extends JpaRepository<HumidityStatistics, Long> {
+    Optional<HumidityStatistics> findByDeviceIdAndStatDateAndStatHour(
+            String deviceId,
+            LocalDate statDate,
+            int statHour
+    );
 }
