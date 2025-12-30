66851863d4c3c3872854934ad17431b2db093fae
sangyong <bagsangyong34@gmail.com>
2025-12-30 17:58:54 +0900

    refactor:불필요한 쿼리 메서드 제거

--- src/main/java/com/smarthome/smart_home_iot/repository/mongo/TemperatureDocumentRepository.java
+++ src/main/java/com/smarthome/smart_home_iot/repository/mongo/TemperatureDocumentRepository.java
@@ -2,15 +2,7 @@ package com.smarthome.smart_home_iot.repository.mongo;
 
 import com.smarthome.smart_home_iot.document.TemperatureDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 public interface TemperatureDocumentRepository extends MongoRepository<TemperatureDocument, String> {
-    // isProcessed가 false인 데이터가 있는지 확인(집계 처리 할 데이터가 있는지 확인)
     boolean existsByIsProcessedFalse();
-
-    @Query("{ 'createdAt': { $gte: ?0, $lte: ?1 }, 'isProcessed': false }")
-    List<TemperatureDocument> findByCreatedAtBetweenAndIsProcessedFalse(LocalDateTime startTime, LocalDateTime endTime);
 }
