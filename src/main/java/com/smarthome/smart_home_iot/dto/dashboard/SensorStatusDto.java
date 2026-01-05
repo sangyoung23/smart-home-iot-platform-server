a200bbeb64f45d6264c693971fcce0361c7a08da
sangyong <bagsangyong34@gmail.com>
2026-01-05 16:09:43 +0900

    feature: dashboard 센서 요약 데이터 조회용 dto 구성

---
+++ src/main/java/com/smarthome/smart_home_iot/dto/dashboard/SensorStatusDto.java
@@ -0,0 +1,18 @@
+package com.smarthome.smart_home_iot.dto.dashboard;
+
+import com.smarthome.smart_home_iot.domain.sensor.SensorType;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class SensorStatusDto {
+
+    private SensorType sensorType;
+    private boolean normal;                 // true: 정상, false: 비정상
+    private String statusMessage;           // 상태 설명 (선택사항)
+    private LocalDateTime lastDataTime;     // 마지막 집계 시간
+    private Double currentValue;            // 현재 값 (평균값)
+}
